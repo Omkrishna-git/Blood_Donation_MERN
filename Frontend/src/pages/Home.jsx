4afc57ce6ff959da8414477f4cb9907f98f5f505
Onkar <omdpatil07@gmail.com>
2024-10-06 20:47:07 +0530

    frontend page done git add .

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,16 +1,28 @@
-import Featured from "../components/Featured"
-import Hero from "../components/Hero"
-import Navbar from "../components/Navbar"
-
+import Contact from "../components/Contact";
+import Featured from "../components/Featured";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import { Element } from "react-scroll";
 
 function Home() {
   return (
     <div>
       <Navbar />
-      <Hero />
-      <Featured />
+      <Element name="hero">
+        <Hero />
+      </Element>
+
+      <Element name="featured">
+        <Featured />
+      </Element>
+      <Element name="contact">
+        <Contact />
+      </Element>
+
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
