5f6aea4358269f3f67393091a0cbc2a36604c546
Onkar <omdpatil07@gmail.com>
2024-10-06 16:33:30 +0530

    Featured section completed..

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Featured from "../components/Featured"
+import Hero from "../components/Hero"
+import Navbar from "../components/Navbar"
+
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Navbar />
+      <Hero />
+      <Featured />
     </div>
   )
 }
